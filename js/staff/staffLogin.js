c0ce8cc93e270873babd27f550c0d9414c6bf09f
g.zambrana <gabrielz022604@gmail.com>
2026-04-20 19:09:08 -0400

    small fix

--- js/staff/staffLogin.js
+++ js/staff/staffLogin.js
@@ -16,7 +16,7 @@ function setMessage(text, color = '') {
 
 async function getUserRole(userId) {
   const { data, error } = await supabase
-    .from('profile')
+    .from('profiles')
     .select('role')
     .eq('id', userId)
     .single();
